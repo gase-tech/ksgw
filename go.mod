a19403e436aa5d33dd2b9ae8293555e44b9ca5c3
Bilal <kocogluubilal@gmail.com>
2021-01-31 05:47:26 +0300

    eureka server connection and i18n (depend environment)

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/imminoglobulib/ksgw
 go 1.15
 
 require (
+	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
 	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
 	github.com/codegangsta/martini v0.0.0-20170121215854-22fa46961aab
 	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab // indirect
+	github.com/go-resty/resty/v2 v2.4.0
 	github.com/google/uuid v1.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/martini-contrib/cors v0.0.0-20141016003011-553b9208d353
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/carlescere/scheduler"
 	"github.com/codegangsta/martini"
+	"github.com/go-resty/resty/v2"
 	uuid2 "github.com/google/uuid"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/martini-contrib/cors"
@@ -20,6 +22,7 @@ import (
 const (
 	// LOG TYPES
 	ReqDetail = "REQUEST_DETAIL"
+	Scheduler = "SCHEDULER"
 
 	// HEADER
 	ReqUuid = "REQ_UUID"
@@ -33,13 +36,33 @@ const (
 	Dev  = "DEV"
 	Test = "TEST"
 	Prod = "PROD"
+
+	// Languages
+	EN = "EN"
+	TR = "TR"
+
+	// Messages
+	ServiceNotFound           = "ServiceNotFound"
+	SuccessfullyFetchOnFile   = "SuccessfullyFetchOnFile"
+	SuccessfullyFetchOnEureka = "SuccessfullyFetchOnEureka"
+	GenericError              = "GenericError"
 )
 
 type ApplicationConfig struct {
+	// EN, TR
+	Language string `envconfig:"LANGUAGE" default:"EN"`
 	// StaticFile, EUREKA, CONSUL -> default => StaticFile
 	LocatorSource string `envconfig:"LOCATOR_SOURCE" default:"STATIC_FILE"`
 	// default => locators.json
 	LocatorFilePath string `envconfig:"LOCATOR_FILE_PATH" default:"locators.json"`
+	// default => 30 second
+	FetchLocatorsSecond int `envconfig:"FETCH_LOCATORS_SECOND" default:"30"`
+	// default => http://localhost:8090/eureka/apps
+	EurekaUrl string `envconfig:"EUREKA_URL" default:"http://localhost:8090/eureka/apps"`
+	// default => ""
+	EurekaUsername string `envconfig:"EUREKA_USERNAME"`
+	// default => ""
+	EurekaPassword string `envconfig:"EUREKA_PASSWORD"`
 	// default => 60
 	TimeOut string `envconfig:"TIME_OUT" default:"60"`
 	// DEV, TEST, PROD -> default => 60
@@ -67,6 +90,8 @@ type LocatorFile struct {
 
 var locators []Locator
 var applicationConfig ApplicationConfig
+var client = resty.New()
+var i18n map[string]string
 
 func init() {
 	err := envconfig.Process("", &applicationConfig)
@@ -75,6 +100,36 @@ func init() {
 		panic(err)
 	}
 
+	loggingConfiguration()
+
+	i18nConfiguration()
+}
+
+func i18nConfiguration() {
+	var jsonFile *os.File
+	var err error
+	if applicationConfig.Language == EN {
+		jsonFile, err = os.Open("EN.json")
+	} else if applicationConfig.Language == TR {
+		jsonFile, err = os.Open("TR.json")
+	} else {
+		jsonFile, err = os.Open("EN.json")
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	err = json.Unmarshal(byteValue, &i18n)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loggingConfiguration() {
 	log.SetFormatter(&log.JSONFormatter{})
 
 	log.SetOutput(os.Stdout)
@@ -119,27 +174,39 @@ func genericHandler() func(http.ResponseWriter, *http.Request, martini.Params) {
 		if len(splitedPath) > 0 {
 			reqPrefix := splitedPath[0]
 
+			var equivalentLocator *Locator
 			for _, locator := range locators {
 				if locator.Prefix == reqPrefix {
-					remote, err := url.Parse(locator.Urls[0])
-					if err != nil {
-						panic(err)
-					}
+					equivalentLocator = &locator
+					break
+				}
+			}
 
-					proxy := prepareProxy(remote)
-
-					redirectPath := getRedirectPath(splitedPath)
-					uuid := uuid2.New().String()
-
-					r.URL.Path = redirectPath
-					// TODO: auth işleminden sonra currentUser header ı eklenmeli
-					r.Header.Add("deneme", "bilal headerrr")
-					r.Header.Add(ReqUuid, uuid)
+			if equivalentLocator != nil {
+				remote, err := url.Parse(equivalentLocator.Urls[0])
+				if err != nil {
 					log.WithFields(log.Fields{
 						"type": ReqDetail,
-					}).Info(r)
-					proxy.ServeHTTP(w, r)
+					}).Error(err)
 				}
+
+				proxy := prepareProxy(remote)
+
+				redirectPath := getRedirectPath(splitedPath)
+				uuid := uuid2.New().String()
+
+				r.URL.Path = redirectPath
+				// TODO: auth işleminden sonra currentUser header ı eklenmeli
+				r.Header.Add("deneme", "bilal headerrr")
+				r.Header.Add(ReqUuid, uuid)
+				log.WithFields(log.Fields{
+					"type": ReqDetail,
+				}).Info(r)
+				proxy.ServeHTTP(w, r)
+			} else {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusNotFound)
+				_, _ = w.Write(generateGenericErrorJsonStr(i18n[ServiceNotFound]))
 			}
 		}
 	}
@@ -171,29 +238,73 @@ func prepareProxy(remote *url.URL) *httputil.ReverseProxy {
 
 	proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, err error) {
 		log.Error(err)
+		writer.Header().Set("Content-Type", "application/json")
 		writer.WriteHeader(http.StatusBadGateway)
 		errStr := err.Error() + " Timeout: " + applicationConfig.TimeOut + " second."
-		_, _ = writer.Write([]byte(errStr))
+		_, _ = writer.Write(generateGenericErrorJsonStr(errStr))
 	}
 
 	return proxy
 }
 
 func fillLocators() {
+	var receiveLocatorStrategy func()
 	if applicationConfig.LocatorSource == StaticFile {
-		readFile()
+		receiveLocatorStrategy = func() { receiveLocatorsOnFile() }
 	} else if applicationConfig.LocatorSource == Eureka {
-		// eureka
+		receiveLocatorStrategy = func() { receiveLocatorsOnEureka() }
 	} else if applicationConfig.LocatorSource == Consul {
 		// consul
 	}
+
+	_, _ = scheduler.Every(applicationConfig.FetchLocatorsSecond).Seconds().Run(receiveLocatorStrategy)
 }
 
-func readFile() {
+func receiveLocatorsOnEureka() {
+	request := client.R()
+	request.SetHeader("Content-Type", "application/json")
+	request.SetHeader("Accept", "application/json")
+	request.SetResult(&EurekaRegisteredServiceInfos{})
+
+	if applicationConfig.EurekaUsername != "" && applicationConfig.EurekaPassword != "" {
+		request.SetBasicAuth(applicationConfig.EurekaUsername, applicationConfig.EurekaPassword)
+	}
+
+	response, err := request.Get(applicationConfig.EurekaUrl)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"type": Scheduler,
+		}).Error(err)
+	}
+
+	result := response.Result().(*EurekaRegisteredServiceInfos)
+
+	locators = make([]Locator, len(result.Application.Apps))
+	for i, service := range result.Application.Apps {
+		locator := &Locator{}
+		lower := strings.ToLower(service.Name)
+		locator.Prefix = lower
+
+		urls := make([]string, len(service.Instances))
+		for i, instance := range service.Instances {
+			urls[i] = "http://" + instance.Hostname + ":" + strconv.FormatInt(int64(instance.Port.Value), 10)
+		}
+		locator.Urls = urls
+		locators[i] = *locator
+	}
+	log.WithFields(log.Fields{
+		"type": Scheduler,
+	}).Debugf(i18n[SuccessfullyFetchOnEureka])
+}
+
+func receiveLocatorsOnFile() {
 	jsonFile, err := os.Open(applicationConfig.LocatorFilePath)
 
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"type": Scheduler,
+		}).Error(err)
 	}
 
 	defer jsonFile.Close()
@@ -207,4 +318,56 @@ func readFile() {
 	}
 
 	locators = fileObj.Locators
+	log.WithFields(log.Fields{
+		"type": Scheduler,
+	}).Debugf(i18n[SuccessfullyFetchOnFile])
+}
+
+type EurekaRegisteredServiceInfos struct {
+	Application EurekaRegisteredApp `json:"applications"`
+}
+
+type EurekaRegisteredApp struct {
+	Version string                      `json:"versions__delta"`
+	Hash    string                      `json:"apps__hashcode"`
+	Apps    []EurekaRegistryApplication `json:"application"`
+}
+
+type EurekaRegistryApplication struct {
+	Name      string                   `json:"name"`
+	Instances []EurekaRegistryInstance `json:"instance"`
+}
+
+type EurekaRegistryInstance struct {
+	InstanceId       string             `json:"instanceId"`
+	Hostname         string             `json:"hostname"`
+	App              string             `json:"app"`
+	IpAddress        string             `json:"ipAddr"`
+	Status           string             `json:"status"`
+	OverriddenStatus string             `json:"overriddenStatus"`
+	Port             EurekaRegisterInfo `json:"port"`
+	SecurePort       EurekaRegisterInfo `json:"securePort"`
+	CountryId        int                `json:"countryId"`
+}
+
+type EurekaRegisterInfo struct {
+	Value    int    `json:"$"`
+	IsActive string `json:"@enabled"`
+}
+
+func generateGenericErrorJsonStr(msg string) []byte {
+	model := GenericErrorModel{Msg: msg}
+
+	jsStr, err := json.Marshal(model)
+
+	if err != nil {
+		log.Error(err)
+		return []byte(i18n[GenericError])
+	}
+
+	return jsStr
+}
+
+type GenericErrorModel struct {
+	Msg string `json:"msg"`
 }
